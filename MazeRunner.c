d95a10323cab57f5a4197b510188f8c1680f14d5
David L <117825871+Davvezn@users.noreply.github.com>
2024-09-30 11:42:55 +0200

    RED WORKS
    did gojo red and it works but needs Arrowhead treatment (nerf)

--- MazeRunner.c
+++ MazeRunner.c
@@ -6,21 +6,16 @@
 
 #define PLAYER_MAX_ARROWS 500
 #define MAX_MELEES 500
-/*
-typedef struct Player {
-    int position_x;
-    int position_y;
-    float velocity_x;
-    float velocity_y;
-    int radius;
-} Player; */
+#define MAX_REDS 90
+#define MAX_BLUES 100
+#define MAX_PURPLES 50
 
 typedef struct Bullet {
     Vector2 position;
     Vector2 speed;
     bool active;
     Color color;
-} arrow;
+} Projetile;
 
 typedef struct Melee {
     Vector2 position;
@@ -30,7 +25,23 @@ typedef struct Melee {
     Color color;
 } melee;
 
-void shootArrow(arrow arrows[],Vector2 position, Vector2 direction) {
+typedef struct Red {
+    Vector2 position;
+    Vector2 speed;
+    bool active;
+    int bounces;
+}Red;
+
+typedef struct Enemies {
+    int HP;
+    int Attack;
+    Vector2 position;
+    Vector2 speed;
+    Color color;
+    bool active; 
+} enemies;
+
+void shootArrow(Projetile arrows[],Vector2 position, Vector2 direction) {
     for (int i = 0; i < PLAYER_MAX_ARROWS; i++) {
         if (!arrows[i].active) {
             arrows[i].position = position;
@@ -41,20 +52,20 @@ void shootArrow(arrow arrows[],Vector2 position, Vector2 direction) {
     }
 }
 
-void updateArrow(arrow arrows[]) {
+void updateArrow(Projetile arrows[]) {
     for (int i = 0; i < PLAYER_MAX_ARROWS; i++) {
         if (arrows[i].active) {
             arrows[i].position.x += arrows[i].speed.x;
             arrows[i].position.y += arrows[i].speed.y;
 
-            if (arrows[i].position.x < 0 || arrows[i].position.x > WINDOWWIDTH || arrows[i].position.y < 0 || arrows[i].position.y > WINDOWHEIGHT) {
+            if (arrows[i].position.x < 0 || arrows[i].position.x > WINDOWWIDTH || arrows[i].position.y < 0 || arrows[i].position.y > WINDOWHEIGHT) { //if outside window
                 arrows[i].active = false;
             }
         }
     }
 }
 
-void DrawArrow(arrow arrows[]) {
+void DrawArrow(Projetile arrows[]) {
     for (int i = 0; i< PLAYER_MAX_ARROWS; i++) {
         if (arrows[i].active) {
             DrawCircleV(arrows[i].position, 5, GREEN);
@@ -62,28 +73,84 @@ void DrawArrow(arrow arrows[]) {
     }
 }
 
-void MeleeAttack(melee melees[], Vector2 position, Vector2 direction, Vector2 size/*might be able to add enemies etc here later*/) {
-    for ( int i = 0; i < MAX_MELEES; i++) {
-        if (!melees[i].active) {
-            melees[i].position = position;  
-            melees[i].active = true;
-            melees[i].size = size;
-
-            Rectangle meleeArea = {position.x - size.x / 2, position.y - size.y / 2, size.x, size.y };
-
+void ShootRed(Red red[], Vector2 position, Vector2 direction){
+    for (int i = 0; i< MAX_REDS; i++) {
+        if (!red[i].active) {
+            red[i].position = position;
+            red[i].speed = Vector2Scale(Vector2Normalize(direction), 10.0f);
+            red[i].active = true;
+            red[i].bounces = 0;
             break;
         }
     }
 }
 
-void updateAttack() {
-    //duration etc 
+void updateRed(Red red[]) {
+    for (int i = 0; i < MAX_REDS; i++) {
+        if (red[i].active == true) {
+            red[i].position.x += red[i].speed.x;
+            red[i].position.y += red[i].speed.y;
+
+            
+            if (red[i].position.x < 0 || red[i].position.x > WINDOWWIDTH) {
+                 red[i].speed.x *= -1;
+                red[i].bounces++;
+            }
+
+            if (red[i].position.y < 0 || red[i].position.y > WINDOWHEIGHT) {
+                red[i].speed.y *= -1;
+                red[i].bounces++;
+            }
+            
+            if (red[i].bounces >= 10) {
+                red[i].active = false;
+            }
+        }
+    }
+} 
+
+void DrawRed(Red red[]) {
+    for (int i = 0; i < MAX_REDS; i++) {
+        if(red[i].active) {
+            DrawCircleV(red[i].position, 20, RED);
+        }
+    }
+}
+
+void MeleeAttack(melee melees[], Vector2 position, Vector2 direction, Vector2 size/*might be able to add enemies etc here later*/) {
+    for ( int i = 0; i < MAX_MELEES; i++) {
+        if (!melees[i].active) {
+
+            Vector2 normalizedDirection = Vector2Normalize(direction);
+
+            melees[i].position.x = position.x + normalizedDirection.x * (size.y / 3 + 1);
+            melees[i].position.y = position.y + normalizedDirection.y * (size.y / 3 + 1);
+
+            melees[i].direction = direction;  
+            melees[i].active = true;
+            melees[i].size = size;
+            break;
+        }
+    }
+}
+
+void updateAttack(melee melees[]) {
+    for (int i = 0; i < MAX_MELEES; i++) {
+        if (melees[i].active) {
+
+        }
+    }
 }
 
 void DrawAttackAoe(melee melees[]) {
     for (int i = 0; i < MAX_MELEES; i++) {
         if (melees[i].active) {
-            DrawRectangleV(melees[i].position,  melees[i].size, BLUE);
+            Rectangle meleeArea = { melees[i].position.x, melees[i].position.y, melees[i].size.x, melees[i].size.y};
+
+            Vector2 origin = {melees[i].size.x / 2.0f, melees[i].size.y / 2.0f};
+
+            float rotation = atan2f(melees[i].direction.y, melees[i].direction.x) * (180.0f /PI);
+            DrawRectanglePro(meleeArea, origin, rotation, BLUE);
         }
     }
 }
@@ -100,10 +167,16 @@ int main() {
     double dodgeTime = 0.0f;
     double dodgeDuration = 0.2;
 
+
+
     // attack logics
+    float meleeDuration = 0.5f;
+
     melee melees[MAX_MELEES] = {0};
 
-    arrow arrows[PLAYER_MAX_ARROWS] = {0};
+    Projetile arrows[PLAYER_MAX_ARROWS] = {0};
+
+    Red red[MAX_REDS] = {0};
 
     InitWindow( WINDOWWIDTH, WINDOWHEIGHT, "Maze runner");
     SetTargetFPS(60);
@@ -111,8 +184,9 @@ int main() {
         Vector2 mousePosition = GetMousePosition();
         Vector2 BulletDirection = Vector2Normalize(Vector2Subtract(mousePosition, playerPosition));
         Vector2 arrowSpeed = {BulletDirection.x * 10.0f, BulletDirection.y * 10.0};
-
+        Vector2 MeleeDirection = Vector2Normalize(Vector2Subtract(mousePosition, playerPosition));
         updateArrow(arrows);
+        updateRed(red);
 
         double currentTime = GetTime();
 
@@ -141,9 +215,13 @@ int main() {
             shootArrow(arrows, playerPosition, arrowSpeed);
         }
 
+        if (IsKeyReleased(KEY_E)) {
+            ShootRed(red, playerPosition, arrowSpeed);
+        }
+
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-            Vector2 meleeSize = { 50.0f, 20.0f};
-            MeleeAttack(melees, playerPosition, direction, meleeSize);
+            Vector2 meleeSize = { 30.0f, 70.0f}; // size of melee attack
+            MeleeAttack(melees, playerPosition, MeleeDirection, meleeSize);
         }
 
         // dodge logic
@@ -180,13 +258,15 @@ int main() {
         // add an larger if statement restricting melee attacks when dodging or shooting arrows
 
 
+
         BeginDrawing();
         ClearBackground(BLACK);
 
 
-        DrawCircleV(playerPosition, 12, RED);
+        DrawCircleV(playerPosition, 12, WHITE); // player
         DrawArrow(arrows);
         DrawAttackAoe(melees);
+        DrawRed(red);
         EndDrawing();
     }
 
