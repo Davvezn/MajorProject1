5c0da0f4c47ef4cca421013c9cf7914dd2c4b5c9
David L <117825871+Davvezn@users.noreply.github.com>
2024-10-08 13:19:01 +0200

    Melee works with features
    Melee is now fully functioning with a duration, aoe and damage set( might need tweaking in future).

--- MazeRunner.c
+++ MazeRunner.c
@@ -1,5 +1,15 @@
 #include "raylib.h"
 #include <raymath.h>
+#include <stdio.h>
+#include <math.h>
+
+typedef struct {
+    float x;
+    float y;
+    float w;
+    float h;
+    float rot;
+} rotated_rect_t;
 
 #define WINDOWWIDTH 800
 #define WINDOWHEIGHT 600
@@ -10,7 +20,15 @@
 #define MAX_BLUES 100
 #define MAX_PURPLES 50
 
-#define MAX_ZOMBIES 80
+#define MAX_ZOMBIES 100
+
+typedef struct Player {
+    int HP;
+    bool active;
+    Vector2 position;
+    Vector2 speed;
+    Color color;
+} Player;
 
 typedef struct Bullet {
     Vector2 position;
@@ -23,6 +41,7 @@ typedef struct Melee {
     Vector2 position;
     Vector2 direction;
     Vector2 size;
+    float lifetime;
     bool active;
     Color color;
 } melee;
@@ -42,6 +61,32 @@ typedef struct Enemies {
     Color color;
     bool active; 
 } enemies;
+
+void DrawRotatedRectangle(rotated_rect_t rect, Color color) {
+    Vector2 origin = { rect.x + rect.w / 2.0f, rect.y + rect.h / 2.0f};
+    DrawRectanglePro((Rectangle){ rect.x, rect.y, rect.w, rect.h}, origin, rect.rot, color); // temporary to make a mindmap
+}
+
+bool IsCircleInRotatedRect(Vector2 circlePos, float radius, rotated_rect_t rect) {
+    Vector2 localCirclePos = Vector2Subtract(circlePos, (Vector2){ rect.x + rect.w / 2.0f, rect.y + rect.h / 2.0f });
+
+    float cosine = cosf(-rect.rot * (PI / 180.0f));
+    float sine = sinf(-rect.rot * (PI / 180.0f));
+
+    float rotatedX = localCirclePos.x * cosine - localCirclePos.y * sine;
+    float rotatedY = localCirclePos.y * sine -localCirclePos.y * cosine;
+
+    float halfWidth = rect.w / 2.0;
+    float halfHeight = rect.h / 2.0f;
+
+    float closestX = fmax(-halfWidth, fmin(rotatedX, halfWidth));
+    float closestY = fmax(-halfHeight, fmin(rotatedY, halfHeight));
+
+    float distanceX = rotatedX - closestX;
+    float distanceY = rotatedY - closestY;
+
+    return (distanceX * distanceX + distanceY * distanceY) <= (radius * radius); 
+}
 
 Vector2 getRandomSpawnLocation();
 
@@ -88,9 +133,10 @@ void updateZombies(enemies Zombie[], Vector2 playerPosition, float zombieSpeed, 
             if (Zombie[i].position.y > WINDOWHEIGHT - 10) {
                 Zombie[i].position.y =  WINDOWHEIGHT - 10;
             }
+
             for (int j = 0; j < PLAYER_MAX_ARROWS; j++) {
                 if (arrows[j].active && CheckCollisionCircles(Zombie[i].position, 20, arrows[j].position, 10)) {
-                    Zombie[i].HP -= 10;
+                    Zombie[i].HP -= 20;
                     arrows[j].active = false;
                 }
             }
@@ -202,18 +248,49 @@ void MeleeAttack(melee melees[], Vector2 position, Vector2 direction, Vector2 si
 
             melees[i].position.x = position.x + normalizedDirection.x * (size.y / 3 + 1);
             melees[i].position.y = position.y + normalizedDirection.y * (size.y / 3 + 1);
-
+            
+            melees[i].lifetime = 0.2;
             melees[i].direction = direction;  
             melees[i].active = true;
             melees[i].size = size;
+            /*
+            DrawRectangle(melees[i].position.x - melees[i].size.x / 2.0f,
+                melees[i].position.y - melees[i].size.y / 2.0f,
+                melees[i].size.x,
+                melees[i].size.y,
+                YELLOW);*/
             break;
         }
     }
 }
 
-void updateAttack(melee melees[]) {
+void updateAttack(melee melees[], enemies Zombie[]) {
     for (int i = 0; i < MAX_MELEES; i++) {
         if (melees[i].active) {
+            float rotation = atan2f(melees[i].direction.y, melees[i].direction.x) * (180.0f / PI);
+            rotated_rect_t meleeRect = {
+                .x = melees[i].position.x,
+                .y = melees[i].position.y,
+                .w = melees[i].size.x,
+                .h = melees[i].size.y,
+                .rot = rotation
+            };
+            melees[i].lifetime -= GetFrameTime();
+
+            if (melees[i].lifetime <= 0.0) {
+                melees[i].active = false;
+            }
+            
+
+            for (int z = 0; z < MAX_ZOMBIES; z++) {
+                if (IsCircleInRotatedRect(Zombie[z].position, 1, meleeRect)) {
+                    Zombie[z].HP -= 50;
+                    if (Zombie[z].HP <= 0) {
+                        Zombie[z].active = false; 
+                    } 
+                }
+            }
+
 
         }
     }
@@ -222,11 +299,9 @@ void updateAttack(melee melees[]) {
 void DrawAttackAoe(melee melees[]) {
     for (int i = 0; i < MAX_MELEES; i++) {
         if (melees[i].active) {
-            Rectangle meleeArea = { melees[i].position.x, melees[i].position.y, melees[i].size.x, melees[i].size.y};
-
-            Vector2 origin = {melees[i].size.x / 2.0f, melees[i].size.y / 2.0f};
-
             float rotation = atan2f(melees[i].direction.y, melees[i].direction.x) * (180.0f /PI);
+            Rectangle meleeArea = { melees[i].position.x, melees[i].position.y, melees[i].size.x, melees[i].size.y};
+            Vector2 origin = {melees[i].size.x / 2.0f, melees[i].size.y / 2.0f};
             DrawRectanglePro(meleeArea, origin, rotation, BLUE);
         }
     }
@@ -258,9 +333,9 @@ Vector2 getRandomSpawnLocation() {
 
 int main() {
 
-    Vector2 playerPosition = { WINDOWWIDTH/2.0f, WINDOWHEIGHT/2.0f };
+    Player player_one = {100, true, { WINDOWWIDTH/2, WINDOWHEIGHT/2}, {7.5f,7.5f}, WHITE};
     float BaseSpeed = 7.5f;
-    float playerSpeed = BaseSpeed;
+    float SprintMultiplier = 1.5f;
 
     // dodge values
     float dodgeDistance = 10.0f;
@@ -292,91 +367,90 @@ int main() {
 
     InitWindow( WINDOWWIDTH, WINDOWHEIGHT, "Maze runner");
     SetTargetFPS(60);
-
-
     while (!WindowShouldClose()) {
         Vector2 mousePosition = GetMousePosition();
-        Vector2 BulletDirection = Vector2Normalize(Vector2Subtract(mousePosition, playerPosition));
+        Vector2 BulletDirection = Vector2Normalize(Vector2Subtract(mousePosition, player_one.position));
         Vector2 arrowSpeed = {BulletDirection.x * 10.0f, BulletDirection.y * 10.0};
-        Vector2 MeleeDirection = Vector2Normalize(Vector2Subtract(mousePosition, playerPosition));
+        Vector2 MeleeDirection = Vector2Normalize(Vector2Subtract(mousePosition, player_one.position));
 
         if (zombiesToSpawn >= 0) {
-            spawnZombies(Zombie, playerPosition, Vector2Zero(), zombiesToSpawn);
+            spawnZombies(Zombie, player_one.position, Vector2Zero(), zombiesToSpawn);
             zombiesToSpawn = 1;
         }
         //updates
+        updateAttack(melees, Zombie);
         updateArrow(arrows);
         updateRed(red);
-        updateZombies(Zombie, playerPosition, Zombie_Speed, arrows, red);
+        updateZombies(Zombie, player_one.position, Zombie_Speed, arrows, red);
 
         double currentTime = GetTime();
 
         if (IsKeyDown(KEY_LEFT_SHIFT)) {
-            playerSpeed = BaseSpeed * 1.5f;
+            player_one.speed.x = BaseSpeed * SprintMultiplier;
+            player_one.speed.y = BaseSpeed * SprintMultiplier;
         } else {
-            playerSpeed = BaseSpeed;
+            player_one.speed.x = BaseSpeed;
+            player_one.speed.y = BaseSpeed;
         }
 
         Vector2 direction = {0.0f, 0.0f};
         //movement logic for player_one
         if(IsKeyDown(KEY_W)) {
-            direction.y -= playerSpeed;
+            direction.y -= player_one.speed.y;
         }
         if(IsKeyDown(KEY_S)) {
-            direction.y += playerSpeed;
+            direction.y += player_one.speed.y;
         }
         if(IsKeyDown(KEY_A)) {
-            direction.x -= playerSpeed;
+            direction.x -= player_one.speed.x;
         }
         if(IsKeyDown(KEY_D)) {
-            direction.x += playerSpeed;
+            direction.x += player_one.speed.x;
         }
 
         if(IsMouseButtonReleased(MOUSE_BUTTON_RIGHT)) {
-            shootArrow(arrows, playerPosition, arrowSpeed);
+            shootArrow(arrows, player_one.position, arrowSpeed);
         }
 
         if (IsKeyReleased(KEY_E)) {
             if (currentTime >= lastRed + redCooldownTime) {
-                ShootRed(red, playerPosition, arrowSpeed);
+                ShootRed(red, player_one.position, arrowSpeed);
                 lastRed = currentTime;
             }
         }
 
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
             Vector2 meleeSize = { 30.0f, 70.0f}; // size of melee attack
-            MeleeAttack(melees, playerPosition, MeleeDirection, meleeSize);
+            MeleeAttack(melees, player_one.position, MeleeDirection, meleeSize);
         }
 
         // dodge logic
         if (IsKeyPressed(KEY_SPACE) && Vector2Length(direction) > 0) { // checks if space is active addiotinally if any other direction input is being used
             isDodging = true;
             dodgeTime = currentTime;
-            playerPosition.x += direction.x * dodgeDistance;
-            playerPosition.y += direction.y * dodgeDistance;
+            player_one.position.x += direction.x * dodgeDistance;
+            player_one.position.y += direction.y * dodgeDistance;
         }
-
         if (isDodging && (currentTime - dodgeTime) > dodgeDuration) {
             isDodging = false;
         }
-
         if (!isDodging) {
-            playerPosition.x += direction.x * playerSpeed * GetFrameTime();
-            playerPosition.y += direction.y * playerSpeed * GetFrameTime();
+            player_one.position.x += direction.x * player_one.speed.x * GetFrameTime();
+            player_one.position.y += direction.y * player_one.speed.y * GetFrameTime();
         }
 
         // border collision check
-        if (playerPosition.x < 0 + 12) {
-            playerPosition.x = 0 + 12;
+        if (player_one.position.x < 0 + 12) {
+            player_one.position.x = 0 + 12;
         }
-        if (playerPosition.x >  WINDOWWIDTH - 12) {
-            playerPosition.x = WINDOWWIDTH - 12;
+        if (player_one.position.x >  WINDOWWIDTH - 12) {
+            player_one.position.x = WINDOWWIDTH - 12;
         }
-        if (playerPosition.y < 0 + 12) {
-            playerPosition.y = 0 + 12;
+        if (player_one.position.y < 0 + 12) {
+            player_one.position.y = 0 + 12;
         }
-        if (playerPosition.y > WINDOWHEIGHT - 12) {
-            playerPosition.y =  WINDOWHEIGHT - 12;
+        if (player_one.position.y > WINDOWHEIGHT - 12) {
+            player_one.position.y =  WINDOWHEIGHT - 12;
         }
         // Schematic for stamina / mana bar
         float cooldownPercentage = (currentTime - lastRed) / redCooldownTime;
@@ -386,9 +460,9 @@ int main() {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        drawZombie(Zombie);
         DrawAttackAoe(melees);
-        DrawCircleV(playerPosition, 12, WHITE); // player
+        drawZombie(Zombie);
+        DrawCircleV(player_one.position, 12, WHITE); // player
         DrawArrow(arrows);
         DrawRed(red);
         DrawRectangle(50, 50, 20, (int)(200 * cooldownPercentage), StaminaBarColor); 
